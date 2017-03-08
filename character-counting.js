044a78d46283dd4908901e0e63ae00382726ace4
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-08 20:19:00 +0000

    created function that counts number of each letter in a string

---
+++ character-counting.js
@@ -0,0 +1,20 @@
+function countLetters(input) {
+  var inputString = input.split(" ").join("").toLowerCase();  // spaces shouldn't be counted
+
+  var outputObject = {};
+
+  for (var i = 0; i < inputString.length; i++) {
+    character = inputString[i];
+
+    if (outputObject[character]) { // if there's a number there
+      outputObject[character] += 1;
+    }
+    else {
+      outputObject[character] = 1;
+    }
+
+  }
+  return outputObject;
+};
+
+console.log(countLetters("Lighthouse in the house"));
